d8ce414dd392f400d2c724a64fbe30c942f2a1cf
Gabriel Kenneth Mariñas <kenneth@lexagle.com>
2024-02-05 03:08:23 +0800

    Create CoverImage component
    Summary:
    - This component is used to create a cover image for a content.

---
+++ src/component/CoverImage/CoverImage.tsx
@@ -0,0 +1,33 @@
+import "./CoverImage.styles.css"
+
+export interface CoverImageProps {
+	/**
+	 * Must include file extension, e.g. `foo.png`
+	 */
+	name: string;
+	alt: string;
+    width?: number;
+    height?: number;
+}
+
+const CoverImage = ({ name, alt, width, height }: CoverImageProps) => (
+    <div className="CoverImage__figure">
+        {width && height ? (
+            <img
+                className="CoverImage__image"
+                src={`../../../resources/${name}`}
+                alt={alt}
+                width={width}
+                height={height}
+            ></img>    
+        ) : (
+            <img
+                className="CoverImage__image"
+                src={`../../../resources/${name}`}
+                alt={alt}
+            ></img>
+        )}
+    </div>
+);
+
+export default CoverImage;
